cff0d200dc7b71fec03781d4bfdad6576657ef79
kirubel <kirubel@gmail.com>
2022-07-04 13:55:01 -0700

    task

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     for i in reversed(my_list):
-        my_iste = my_list[i]
-        print(my_iste)
+        print('{:d}'.format(i))
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        my_list[idx] = elment
-        return my_list
+        liste = my_list.cop()
+        liste[idx] = element
+        return liste
