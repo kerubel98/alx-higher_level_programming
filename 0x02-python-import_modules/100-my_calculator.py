c7c523c37bbe48ff48bf33510cc982e92138b393
kirubel <kirubel@gmail.com>
2022-07-04 09:21:39 -0700

    task

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1.py import*
+if __name__ == "__maine__":
+    a = int(argv[1])
+    b = int(argv[3])
+    if len(argv) < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    elif argv[2] == "+":
+        print('{} + {} = {}'.format(a, b, add(a, b)))
+    elif arg[2] == "-":
+        print('{} - {} = {}'.format(a, b, sub(a, b)))
+    elif arg[2] == "*":
+        print('{} * {} = {}'.format(a, b, mul(a, b)))
+    elif arg[2] == "/":
+        print('{} - {} = {}'.format(a, b, div(a, b)))
