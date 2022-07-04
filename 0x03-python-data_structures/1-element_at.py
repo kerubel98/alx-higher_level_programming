912381380346a11a6a0a91c429a2b647931c31ff
kirubel <kirubel@gmail.com>
2022-07-04 12:35:49 -0700

    task

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return (None)
-    elif idx > len(my_list):
-        return (None)
+        return None
+    elif idx >= len(my_list):
+        return None
     else:
         for idx in my_list:
-            return (my_list[idx])
+            return my_list[idx]
