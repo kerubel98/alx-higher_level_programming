a9532ac40a9f3b5cf661fd3bc1e9958d5fc993f6
kirubel <kirubel@gmail.com>
2022-07-20 12:45:25 -0700

    task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """Square definition"""
+
+    def __init__(self, size=0):
+        """Constructor
+        Args:
+            size: size of sides
+        Raises:
+            ValueError: if size < 0
+            TypeError: if size is not an integer
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of a square
+        Return:
+            the size squared
+        """
+        return self.__size ** 2
