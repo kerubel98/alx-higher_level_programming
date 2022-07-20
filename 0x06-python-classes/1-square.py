64cd5fd7e06d9428728bcc93a84bbd68c8cd4774
kirubel <kirubel@gmail.com>
2022-07-20 12:35:11 -0700

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """Defines Square class"""
+
+    def __init__(self, size):
+        """ constructor,
+        Args:
+            size: lenght of sides
+        """
+        self.__size = size
