218a5390c424f04627db953a653299f943e377b6
kerubel98 <kirubeltadesse98@gmail.com>
2023-12-19 08:07:40 -0800

    modified square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square clase"""
+
+
+class Square:
+    """Define Square class"""
+
+    def __init__(self, size=0):
+        """ constructor,
+
+        Args:
+            size: lenght of sides
+        """
+
+        self.__size = size
