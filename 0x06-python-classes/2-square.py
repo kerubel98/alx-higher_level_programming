ce0cdcaae25ee5da1dbee6ff7d83dd79e891e018
kerubel98 <kirubeltadesse98@gmail.com>
2023-12-19 08:14:28 -0800

    modfied 2-square"

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square clase"""
+
+
+class Square:
+    """Define Square class"""
+
+    def __init__(self, size=0):
+        """ constructor,
+
+        Args:
+            size: lenght of sides
+
+        Rasises:
+            TypeError: if size is not intiger
+            ValueError: if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
